a9d208ac18df13ca347b99ac893c527014c78672
Eduardo Santiago <eeduardosantiago@gmail.com>
2016-06-30 22:47:02 -0300

    não fiz porra nenhuma ainda

---
+++ index.js
@@ -0,0 +1,31 @@
+#!/usr/bin/env node
+
+
+
+wt('É hora do show porra!!!');
+
+
+function wt(texto){
+	console.log(texto);
+
+}
+
+function inicializar(){
+
+}
+
+function avaliar(){
+
+}
+
+function cruzamento(){
+
+}
+
+function mutacao(){
+
+}
+
+function selecionar(){
+	
+}
